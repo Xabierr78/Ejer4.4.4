baaeb43275033f2f863489e20236e35154232ca2
Xabierr78 <116910820+Xabierr78@users.noreply.github.com>
2023-02-05 19:04:24 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+Console.WriteLine("cuantos alumnos quieres calificar ?");
+
+int x = int.Parse(Console.ReadLine());
+
+
+
+String Resultado = ""; 
+
+for (int i=0;i<x; i++)
+{
+    Console.WriteLine("como se llama el alumno numero:? " + (i+1));
+    string nombre = Console.ReadLine();
+
+
+    Console.WriteLine("Cual ha sido la nota 1 del alumno:" + nombre);
+    double nota1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Cual ha sido la nota 2 del alumno: " + nombre);
+    double nota2 = double.Parse(Console.ReadLine());
+
+    double media = (nota1 + nota2) / 2;
+
+    Resultado += "NOMBRE: " + nombre + "\nNOTA 1: " + nota1 + "\nNOTA 2:" + nota2 + "\nPROMEDIO: " + media + "\n"; 
+}
+
+
+Console.WriteLine(Resultado);
